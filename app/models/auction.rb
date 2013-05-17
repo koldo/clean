16443068b987048921e5a0c6e9ba10a1744c530d
LPV <mymail@gmail.com>
2013-05-17 17:50:36 +0100

    Added hash with possible status

--- app/models/auction.rb
+++ app/models/auction.rb
@@ -1,6 +1,19 @@
 class Auction < ActiveRecord::Base
   attr_accessible :content, :user_id
-  belongs_to :user
+  attr_accessor :possible_status
 
+  belongs_to :user
   validates :user_id, presence: true
+
+	possible_status = {"For Sale" => true, "Sold" => false, "Bought" => false}
+	
+	#getter
+	def status
+		self[:possible_status]
+	end
+
+	#setter
+	def status=(status, value)
+		self[:possible_status[status]] = value
+	end
 end
